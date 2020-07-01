dc932594f5e8f8ef66a71d037c4d679da2f62802
Arique Aguilar <arique1104@gmail.com>
2020-07-01 14:17:14 -0500

    Passing name method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant
-  attr_reader   :opening_time
+  attr_reader   :opening_time,
+                :name
   def initialize(open_time, name)
     @opening_time = open_time
+    @name = name
   end
 end
