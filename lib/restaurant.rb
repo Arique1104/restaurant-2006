a96334b8b644fceed49796cf45c97e7d23e37396
Arique Aguilar <arique1104@gmail.com>
2020-07-01 14:18:20 -0500

    Dishes method passing code

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,13 @@
 class Restaurant
   attr_reader   :opening_time,
-                :name
+                :name,
+                :dishes
+
   def initialize(open_time, name)
     @opening_time = open_time
     @name = name
+    @dishes = []
   end
+
+
 end
